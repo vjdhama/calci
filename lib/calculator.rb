3c5c6d8b771d9eb3630a403f73737e7202d915d7
Vijay Dhama <vjdhama26@gmail.com>
2014-07-20 01:22:09 +0530

    Added 4 very basic and minimal specs and they all pass

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,3 +1,22 @@
-class calculator
+class Calculator
 
+	def initialize
+		@result = 0
+	end
+
+	def add(param)
+		@result += param
+	end
+
+	def subtract(param)
+		@result -= param
+	end
+
+	def multiply(param)
+		@result *= param
+	end
+
+	def divide(param)
+		@result /= param
+	end
 end
--- spec/unit/calculator_spec.rb
+++ spec/unit/calculator_spec.rb
@@ -1,3 +1,26 @@
 require 'spec_helper'
+require "calculator.rb"
 
 
+describe "Calculator" do
+
+	it "will add a number" do
+		calculator = Calculator.new
+		expect(calculator.add 2).to eq 2
+	end
+	
+	it "will subtract a number" do
+		calculator = Calculator.new
+		expect(calculator.subtract 2).to eq -2
+	end
+
+	it "will multiply a number" do
+		calculator = Calculator.new
+		expect(calculator.multiply 2).to eq 0
+	end
+
+	it "will divide a number" do
+		calculator = Calculator.new
+		expect(calculator.divide 2).to eq 0
+	end
+end
